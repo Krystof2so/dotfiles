e1f8f2e6a723751b86705349a7d8563bc03d4384
Krystof2so <christo.2so@gmail.com>
2025-07-16 00:42:23 +0200

    [Feat] add WezTerm
    initial commit

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+-- ##########################################
+-- # Configuration personnalisée de WezTerm #
+-- ##########################################
+
+-- Import de l'API WezTerm (pour accéder aux fonctions et constantes de WezTerm) :
+local wezterm = require("wezterm")
+
+-- Création d'une table qui contiendra les options de configuration.
+-- 'config_builder' : accès à l'autocomplétion.
+local config = wezterm.config_builder()
+
+-- Chargement du module de couleurs
+local colors = require("colors_nord")
+colors.theme_config(config)
+
+-- Définition de la taille initiale des fenêtres du terminal à l'ouverture :
+config.initial_cols = 120 -- 120 caractères de largeur
+config.initial_rows = 28 -- 28 lignes de heuteur
+
+-- Personnalisation de l'apparence :
+config.font_size = 14 -- Taille de la police
+
+-- Scrolling :
+config.scrollback_lines = 3500 -- Nombre de lignes conservées
+config.enable_scroll_bar = true -- Active la scroolbar
+
+-- Obligatoire : retourne l'objet de configuration pour que WezTerm puisse l'appliquer :
+return config
