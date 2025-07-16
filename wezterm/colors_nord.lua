c6ffc805681971fbdcca9aa47f188b27fd260052
Krystof2so <christo.2so@gmail.com>
2025-07-16 08:57:04 +0200

    [Feat] WezTerm
    Change colorscheme -> 'Nord (Goth)'

--- wezterm/colors_nord.lua
+++ wezterm/colors_nord.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 function M.theme_config(config)
-	config.color_scheme = "Nord" -- Théme de couleur (à choisir parmi ceux proposés)
+	config.color_scheme = "Nord (Gogh)" -- Théme de couleur (à choisir parmi ceux proposés)
 
 	config.colors = {
 		scrollbar_thumb = "#81a1c1", -- Couleur du curseur de la scrollbar
