34a606c05355c4f73bd783b098c1b657ff7e8428
Krystof2so <christo.2so@gmail.com>
2025-07-23 09:57:28 +0200

    [Feat] WezTerm
    change light theme : 'Atelierseaside (light) (terminal.sexy)'

--- wezterm/themes/toggle_themes.lua
+++ wezterm/themes/toggle_themes.lua
@@ -4,12 +4,12 @@ local wezterm = require("wezterm")
 wezterm.on("toggle-theme", function(window)
 	local overrides = window:get_config_overrides() or {}
 
-	if overrides.color_scheme == "aikofog (terminal.sexy)" then
+	if overrides.color_scheme == "Atelierseaside (light) (terminal.sexy)" then
 		-- Revenir à ton thème personnalisé
 		overrides.color_scheme = nil -- remet le thème de base (thème Nord appliqué au démarrage)
 	else
 		-- Appliquer un thème clair intégré
-		overrides.color_scheme = "aikofog (terminal.sexy)"
+		overrides.color_scheme = "Atelierseaside (light) (terminal.sexy)"
 	end
 
 	window:set_config_overrides(overrides)
